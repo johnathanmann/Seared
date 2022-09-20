2bd49f96359af20f2def78fd44f565d428ae22ce
unknown <stevewee1979@gmail.com>
2022-09-20 18:59:57 -0500

    Built out time/date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,11 @@
 module.exports = {
-
+  format_time: (date) => {
+    return date.toLocalTimeString();
+  },
+  format_date: (date) => {
+    return (
+      `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}` /
+      `${new Date(date).getFullYear()}`
+    );
+  },
 };

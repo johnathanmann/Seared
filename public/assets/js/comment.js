d8ca232cf43433c00b684ccf75d74485622ca4f3
Johnny <johnathan.mnn@gmail.com>
2022-09-27 21:45:12 -0500

    Linting issues

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -8,25 +8,23 @@ const commentForm = async (event) => {
   const comment_text = document.querySelector('input[name="comment"]').value;
   console.log(comment_text);
 
-  if(comment_text) {
+  if (comment_text) {
     const response = await fetch('/api/comments', {
       method: 'POST',
       body: JSON.stringify({
         recipe_id,
-        comment_text
+        comment_text,
       }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     });
     if (response.ok) {
-      console.log("Comment Posted")
+      console.log('Comment Posted');
     } else {
       alert(response.statusText);
     }
-  };
-}
+  }
+};
 
-document
-  .querySelector('#comment_form')
-  .addEventListener('submit', commentForm);
+document.querySelector('#comment_form').addEventListener('submit', commentForm);
